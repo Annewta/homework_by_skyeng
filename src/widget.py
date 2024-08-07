9257369e87f0045fe85df151be99329a0257b4ff
Annewta <matytsyna2013@mail.ru>
2024-08-07 18:23:07 +0500

    add widget.py

---
+++ src/widget.py
@@ -0,0 +1,31 @@
+import masks
+
+def mask_account_card(data_card: str) -> str:
+    '''Функция, которая умеет обрабатывать информацию как о картах, так и о счетах'''
+    if 'Счет' in data_card:
+        number_score = ''.join(el if el.isdigit() else '' for el in data_card)
+        number_score_mask = masks.get_mask_account(number_score)
+        name_score = ''.join("" if el.isdigit() else el for el in data_card)
+        data_score_mask = name_score + number_score_mask
+        return data_score_mask
+    else:
+        number_card = ''.join(el if el.isdigit() else '' for el in data_card)
+        number_card_mask = masks.get_mask_card_number(number_card)
+        name_card = ''.join("" if el.isdigit() else el for el in data_card)
+        data_card_mask = name_card + number_card_mask
+        return data_card_mask
+
+
+print(mask_account_card('Счет 64686473678894779589'))
+
+def get_date(card_date:str) -> str:
+    '''Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ")'''
+    day = card_date[8:10] + '.'
+    month = card_date[5:7] + '.'
+    year = card_date[:4]
+    total_date = day + month + year
+    return total_date
+
+print(get_date('2024-03-11T02:26:18.671407'))
+
