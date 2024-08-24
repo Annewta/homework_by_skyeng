1c7005ff0966f416d41caf0e5f1c84d936423109
Annewta <matytsyna2013@mail.ru>
2024-08-24 17:14:48 +0500

    add derictirie tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,32 +1,22 @@
 from src.widget import mask_account_card
 from src.widget import get_date
 
-def test_mask_account_card():
+def test_mask_account_card(mask_account_card_fix):
     '''Тесты для проверки, что функция корректно распознает
     и применяет нужный тип маскировки в зависимости от типа входных данных
     (карта или счет)'''
-    pass
+    assert mask_account_card(mask_account_card_fix) == ('Счет **9589', 'Visa Platinum 7000 79** **** 6361', 'Счет **5560')
 
-def test_mask_account_card():
+def test_mask_account_card_type(mask_account_card_fix):
     '''Параметризованные тесты с разными типами карт и счетов
     для проверки универсальности функции'''
-    pass
+    assert mask_account_card(mask_account_card_fix) == ('Счет **9589', 'Visa Platinum 7000 79** **** 6361', 'Maestro 7000 79** **** 6361', 'MasterCard 7158 30** **** 6758', 'Счет **5560')
 
-def test_mask_account_card():
+def test_mask_account_card_error(mask_account_card_fix):
     '''Тестирование функции на обработку некорректных входных данных
     и проверка ее устойчивости к ошибкам'''
-    pass
+    assert mask_account_card(mask_account_card_fix) == 'Некорректные входные данные'
 
-def test_get_date_1():
+def test_get_date(get_date_fix):
     '''Тестирование правильности преобразования даты'''
-    pass
-
-def test_get_date_2():
-    '''Проверка работы функции на различных входных форматах даты,
-    включая граничные случаи и нестандартные строки с датами'''
-    pass
-
-def test_get_date_3():
-    '''Проверка, что функция корректно обрабатывает входные строки,
-    где отсутствует дата'''
-    pass
+    assert get_date(get_date_fix) == ('11.03.2024','11.12.2025', '11.03.2024', '11.03.2024', 'Некорректные данные. Введите дату в формате: ГГГГ-ММ-ДД', '', 'Некорректные данные. Введите дату в формате: ГГГГ-ММ-ДД')
